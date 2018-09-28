f69f87f572cc54880770c4cd47b9dc8f2b616c26
Daniel Silverio <danielsf260796@gmail.com>
2018-09-27 20:51:43 -0500

    Puerto Dinamico

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 app.get('/', (req, res)=>{
     res.send("Hola esto es una prueba")
 })
-app.listen(3000, ()=>{
+app.set('port', process.env.PORT || 3000)
+
+app.listen('port', ()=>{
     console.log('se conecto');
 })
