3a712b141951c1b40480a516e515b127c3e08446
Daniel Silverio <danielsf260796@gmail.com>
2018-09-28 13:27:39 -0500

    Sin .get

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res)=>{
-    res.send("Hola esto es una prueba")
-})
 app.set('port', process.env.PORT || 3000)
 
 app.listen('port', ()=>{
